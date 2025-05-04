85ff4c5a3412cc3e98752a1358a51db539dddf36
gabrielaristevska <ristevskagabriela2@gmail.com>
2025-05-04 21:18:49 +0200

    Added jwt authentication

---
+++ src/main/java/mk/finki/ukim/emt/lab/dto/JwtExceptionResponseDto.java
@@ -0,0 +1,9 @@
+package mk.finki.ukim.emt.lab.dto;
+
+import java.util.Date;
+
+public record JwtExceptionResponseDto(Date timestamp, int status, String error, String message, String path) {
+    public JwtExceptionResponseDto(int status, String error, String message, String path){
+        this(new Date(), status, error, message, path);
+    }
+}
---
+++ src/main/java/mk/finki/ukim/emt/lab/dto/LoginResponseDto.java
@@ -0,0 +1,4 @@
+package mk.finki.ukim.emt.lab.dto;
+
+public record LoginResponseDto(String token) {
+}
